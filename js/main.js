b385d2d2f09d3d03de906794325b53746232b115
Tim Hobson <timhobson85@gmail.com>
2019-12-13 10:03:23 +1100

    cleaned up files, finished layout and refactored code - ready to present

--- js/main.js
+++ js/main.js
@@ -1,149 +1,156 @@
-let gameBoard = [ null, null, null, null, null, null, null, null, null ]
-let turnCount = null
-let player = null
-let playerMark = null
-let gameResult = null
-let gameOver = null
-let $addWin = null
-let winListItem = null
-let playerIMG = null
-let player1IMG = null
-let player2IMG = null
-let player1Name = null
-let player2Name = null
-const $ul = $('ul#wins')
-const $winText = $('#winText')
-const $winName = $('#winName')
-const $scoreBoard = $('.scoreBoard')
+const tictactoe = {
 
-const resetBoard = function () {
-  $('.squares').html('')
-  $('#resetButton').css('width', '200px').animate({width: "50px"}, 500)
-  // $('#resetButton').css('width', '50px')
-  winListItem = null
-  gameOver = false
-  turnCount = 0
-  gameResult = null
-  gameBoard = [ null, null, null, null, null, null, null, null, null ]
-}
+  gameState : {
+    turnCount : null,
+    player : null,
+    playerMark : null,
+    gameResult : null,
+    gameOver : null,
+    winListItem : null,
+    playerIMG : null,
+    $ul : $('ul#wins'),
+    $winText : $('#winText'),
+    $winName : $('#winName')
+  },
 
-const setPlayer= function () { // use object for players and change this
-  if (turnCount % 2 === 0) {
-    player = player1Name
-    playerIMG = player1IMG
-    playerMark = `X`
-  } else {
-    player = player2Name
-    playerIMG = player2IMG
-    playerMark = `O`
-  }
-  $('#turntext').show()
-  $('#turntext').html(`it's ${player}'s turn`).delay(1500).fadeOut()
-  turnCount++
-} //create arrau for playerdetails
+  gameBoard : [
+    null, null, null, null, null, null, null, null, null
+  ],
 
-const checkBoxUsed = function () {
-  if (gameBoard[boxID] === null) {
-    return false
-  } else {
-    console.log(`box is taken, pick again`)
-    return true
-  }
-}
+  winConditionsArray : [
 
-const endGame = function () {
-  if (gameResult === `win`) {
-    winListItem = `${player}-${gameResult}-${playerIMG}`
-    scoreBoard()
-    return gameOver = true
-  } else if (gameResult === `draw`) {
-    winListItem = `${gameResult}`
-    scoreBoard()
-    return gameOver = true
-  } else {
-    return false
-  }
-}
+    [ 0, 1, 2 ],
 
-const placeMark = function () {
-  $(`#${boxID}`).html(`<img src="images/${playerIMG}.png">`)
-  gameBoard[boxID] = playerMark; // pushes the playerMark( X or O ) into the array
-}
+    [ 3, 4, 5 ],
+
+    [ 6, 7, 8 ],
+
+    [ 0, 3, 6 ],
+
+    [ 1, 4, 7 ],
+
+    [ 2, 5, 8 ],
+
+    [ 0, 4, 8 ],
+
+    [ 2, 4, 6 ]
+
+  ], // end of winConditionsArray
+
+  // playerInfo : {
+  //   player1Name: 'Tim', player1IMG: 'spaceship', player2Name: 'Dave', player2IMG: 'pizza'
+  // }, // testing
+
+  playerInfo : {
+    player1Name: null, player1IMG: null, player2Name: null, player2IMG: null
+  },
+
+  resetBoard : function () {
+    $('.squares').html('')
+    $('#resetButton').animate({width: "50px"}, 500)
+    this.gameState.winListItem = null
+    this.gameState.gameOver = false
+    this.gameState.turnCount = 0
+    this.gameState.gameResult = null
+    this.gameBoard = [ null, null, null, null, null, null, null, null, null ]
+  },
+
+  setPlayer : function () { // use object for players and change this
+    if (this.gameState.turnCount % 2 === 0) {
+      this.gameState.player = this.playerInfo.player1Name
+      this.gameState.playerIMG = this.playerInfo.player1IMG
+      this.gameState.playerMark = `X`
+    } else {
+      this.gameState.player = this.playerInfo.player2Name
+      this.gameState.playerIMG = this.playerInfo.player2IMG
+      this.gameState.playerMark = `O`
+    }
+    $('#turntext').show()
+    $('#turntext').html(`it's ${this.gameState.player}'s turn`).delay(1500).fadeOut()
+    this.gameState.turnCount++
+  }, //create arrau for playerdetails
+
+  checkBoxUsed : function () {
+    if (this.gameBoard[boxID] === null) {
+      return false
+    } else {
+      $('#turntext').show()
+      $('#turntext').html(`box is taken, pick again`).delay(1500).fadeOut()
+      return true
+    }
+  },
+
+  endGame : function () {
+    if (this.gameState.gameResult === `win`) {
+      this.gameState.winListItem = `${this.gameState.player}-${this.gameState.gameResult}-${this.gameState.playerIMG}`
+      this.scoreBoard()
+      return this.gameState.gameOver = true
+    } else if (this.gameState.gameResult === `draw`) {
+      this.gameState.winListItem = `${this.gameState.gameResult}`
+      this.scoreBoard()
+      return this.gameState.gameOver = true
+    } else {
+      return false
+    }
+  },
+
+  placeMark : function () {
+    $(`#${boxID}`).html(`<img src="images/${this.gameState.playerIMG}.png">`)
+    this.gameBoard[boxID] = this.gameState.playerMark; // pushes the this.playerMark( X or O ) into the array
+  },
+
+  scoreBoard : function () {
+    if (this.gameState.gameResult === `win`) {
+      this.gameState.$winName.html(`congratulations ${this.gameState.player}`)
+      this.gameState.$winText.html(`you won the game playing as ${this.gameState.playerIMG}`)
+    } else {
+      this.gameState.$winName.html(`what a shame`)
+      this.gameState.$winText.html(`the game ended in a draw`)
+    }
+    $('#resetButton').css('width', '200px')
+    $('.scoreBoard').css('visibility', 'visible')
+    $('#scoreBoardOk').on('click', function () {
+    $('.scoreBoard').css('visibility', 'hidden')
+    })
+    this.winCounter();
+  },
+
+  winCounter : function () {
+    this.gameState.$ul.html(`<li>${this.gameState.winListItem}</li>` + this.gameState.$ul.html())
+    $('#winCounter').show().delay(2500).fadeOut(2000  )
+  },
+
+  winCheck : function () {
+    if (this.gameState.turnCount < 5 ) {
+      return
+    } else {
+      for (let i = 0; i < this.winConditionsArray.length; i++) {
+        let solve = this.winConditionsArray[i]
+        if ( this.gameBoard[solve[0]] && ((this.gameBoard[solve[0]] === this.gameBoard[solve[1]]) && (this.gameBoard[solve[1]] === this.gameBoard[solve[2]]))) {
+          return this.gameState.gameResult = `win`
+        }
+      } if (this.gameState.turnCount === 9) {
+        return this.gameState.gameResult = `draw`
+      }
+    }
+  }, // end of winCheck
+
+} // end of tictactoe
 
 $('.squares').on('click', function () { // this places player number in the corresponing object value.
-  if (gameOver) {
+  if (tictactoe.gameState.gameOver) {
     return
   } else
   boxID = $(this).attr('id'); // records the ID of the square
-    if ( checkBoxUsed( boxID ) ) { // try make this opposite, so if true will send back to middle, or continue with turn.
+    if ( tictactoe.checkBoxUsed( boxID ) ) { // try make this opposite, so if true will send back to middle, or continue with turn.
       return false
     } else {
-    setPlayer();
-    placeMark();
-    winCheck();
-    endGame();
+    tictactoe.setPlayer();
+    tictactoe.placeMark();
+    tictactoe.winCheck();
+    tictactoe.endGame();
   }
 }); // end of onclick
-
-const scoreBoard = function () {
-  if (gameResult === `win`) {
-    $winName.html(`Congratulations ${player}!`)
-    $winText.html(`You won the game playing as ${playerIMG}`)
-  } else {
-    $winName.html(`What a shame!`)
-    $winText.html(`The game ended in a draw`)
-  }
-  $('#resetButton').css('width', '200px')
-  $('.scoreBoard').css('visibility', 'visible')
-  $('#scoreBoardOk').on('click', function () {
-  $('.scoreBoard').css('visibility', 'hidden')
-  })
-  winCounter();
-}
-
-const winCounter = function () {
-  $ul.html(`<li>${winListItem}</li>` + $ul.html())
-  $('#winCounter').show().delay(2500).fadeOut()
-
-}
-
-
-const winConditionsArray = [
-
-  [ 0, 1, 2 ],
-
-  [ 3, 4, 5 ],
-
-  [ 6, 7, 8 ],
-
-  [ 0, 3, 6 ],
-
-  [ 1, 4, 7 ],
-
-  [ 2, 5, 8 ],
-
-  [ 0, 4, 8 ],
-
-  [ 2, 4, 6 ]
-
-]; // end of winConditionsArray
-
-const winCheck = function () {
-
-  if (turnCount < 5 ) {
-    return
-  } else {
-    for (var i = 0; i < winConditionsArray.length; i++) {
-      solve = winConditionsArray[i]
-      if ( gameBoard[solve[0]] && ((gameBoard[solve[0]] === gameBoard[solve[1]]) && (gameBoard[solve[1]] === gameBoard[solve[2]]))) {
-        console.log(`win`);
-        return gameResult = `win`
-      }
-    } if (turnCount === 9) {
-      return gameResult = `draw`
-    }
-  }
-}; // end of winCheck
 
 $('#loadButton').on('click', function () {
   $(this).fadeOut(function () {
@@ -156,27 +163,38 @@ $('#winHover').hover(function () {
   $('#winCounter').toggle()
 })
 
+$('#options').on(`click`, function () {
+  $('#optionsAppear').toggle()
+})
+
+$('#changeNames').on('click', function () {
+  $('#optionsAppear').toggle()
+  $('#wrapper').toggle()
+  $('#nameEntryBox').fadeIn()
+})
+
+
 $('#submitName').on('click', function () { // create a check for no input?
-  player1Name = ( $('#enterPlayer1Name').val() );
-  player2Name = ( $('#enterPlayer2Name').val() );
-  $('#dropBtnP1').html( player1Name );
-  $('#dropBtnP2').html( player2Name );
-  console.log(player1Name);
-  console.log(player2Name);
+  tictactoe.playerInfo.player1Name = ( $('#enterPlayer1Name').val() );
+  tictactoe.playerInfo.player2Name = ( $('#enterPlayer2Name').val() );
+  $('#dropBtnP1').html( tictactoe.playerInfo.player1Name );
+  $('#dropBtnP2').html( tictactoe.playerInfo.player2Name );
   $('#nameEntryBox').fadeOut()
   $('#pickMark').delay(500).fadeIn()
 } );
 
 $('.p2Symbol').on('click', function () {
-  player2IMG = $(this).html();
-  $('#dropBtnP2').html( player2IMG )
-  $('.p2Symbol').hide()
+  $('.p2Symbol').css('backgroundColor', 'whitesmoke')
+  tictactoe.playerInfo.player2IMG = $(this).html();
+  // $('#dropBtnP2').html( tictactoe.playerInfo.player2IMG )
+  // $('.p2Symbol').hide()
+  $(this).css('backgroundColor', 'pink')
 })
 
 $('.p1Symbol').on('click', function () {
-  player1IMG = $(this).html();
-  $('#dropBtnP1').html( player1IMG )
-  $('.p1Symbol').hide()
+  $('.p1Symbol').css('backgroundColor', 'whitesmoke')
+  tictactoe.playerInfo.player1IMG = $(this).html();
+  $(this).css('backgroundColor', 'pink')
 })
 
 $('#symbolOk').on('click', function () { // create if to check picked symbols
@@ -186,5 +204,37 @@ $('#symbolOk').on('click', function () { // create if to check picked symbols
 
 $('#resetButton').on('click', function () {
   $('.scoreBoard').css('visibility', 'hidden')
-  resetBoard()
+  tictactoe.resetBoard()
 }); // end of resetbutton onclick
+
+const deepBrown = {
+
+  randomValue : function( max ){
+    return Math.floor ( Math.random() * max );
+  }, // end of randomNumber
+
+  compareSolve : function () {
+    //search 3 solve conditions
+    for (var i = 0; i < winConditionsArray.length; i++) {
+      var solve = winConditionsArray[i]
+    } for (var j = 0; j < solve.length; j++) {
+        console.log(solve[j])
+        if (solve[j] === true) {
+          //twice then place in third spot in that array
+        }
+
+      }
+  },
+
+
+  thinking : function () {
+    if (this.gameState.turnCount === 1) {
+      //place random
+    }
+    for (var i = 0; i < gameBoard.length; i++) {
+      gameBoard[i]
+      // loop for empty spaces
+    }
+  },//end of makeMove
+
+}// end of deepBrown
